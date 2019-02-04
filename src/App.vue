ac38956ec2ef5eac88d439e913e1f06a8dbb608b
Janos Perge <janos.perge@gmail.com>
2019-02-04 21:26:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Main/>
+  </div>
+</template>
+
+<script>
+import Main from './components/Main'
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './scss/variables.scss';
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
+<style>
+  #app {
+    font-family: 'Raleway', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin-top: 60px;
+  }
+</style>
