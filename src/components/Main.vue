3716d47423d8bceb42a5326bf4c16dfaf51e6ca2
Janos Perge <janos.perge@gmail.com>
2019-02-06 21:40:11 +0100

    complete session 5

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,6 +12,39 @@
               :key="category.id">
               {{ category.name }}
             </button>
+            <h3 class="mt-3 mb-3">Quiz settings</h3>
+            <div class="settings-board">
+              <div class="form-group d-flex align-items-center">
+                <label for="questionPerCategory" style="width: 250px" class="text-left">Possible question per category:</label>
+                <input
+                  id="questionPerCategory"
+                  type="number"
+                  min="10"
+                  max="20"
+                  class="form-control ml-3"
+                  style="width: 70px"
+                  v-model="questionPerCategory"
+                  readonly>
+                <button class="btn btn-info ml-2" @click="questionPerCategoryInc()">+</button>
+                <button class="btn btn-info ml-2" @click="questionPerCategoryDec()">-</button>
+              </div>
+              <div class="form-group d-flex align-items-center">
+                <label for="questionPerCategory" style="width: 250px" class="text-left">Round per category:</label>
+                <input
+                  id="questionForPlay"
+                  type="number"
+                  min="5"
+                  max="10"
+                  class="form-control ml-3"
+                  style="width: 70px"
+                  v-model="questionForPlay"
+                  readonly>
+                <button class="btn btn-info ml-2" @click="questionForPlayInc()">+</button>
+                <button class="btn btn-info ml-2" @click="questionForPlayDec()">-</button>
+              </div>
+              <h5>Possible question per category: <strong>{{questionPerCategory}}</strong></h5>
+              <h5>Round per category: <strong>{{questionForPlay}}</strong></h5>
+            </div>
           </div>
           <div id="questions" v-else>
             <h4>Choosen category: {{selectedCategoryName}}</h4>
@@ -114,10 +147,34 @@ export default {
       possAnswers: [],
       correctAnswer: '',
       userTip: undefined,
-      userAnswers: []
+      userAnswers: [],
+      questionPerCategoryMax: 20,
+      questionPerCategoryMin: 10,
+      questionForPlayMax: 10,
+      questionForPlayMin: 5
     }
   },
   methods: {
+    questionPerCategoryInc: function () {
+      if (this.questionPerCategory <= this.questionPerCategoryMax - 1 && this.questionPerCategory >= this.questionPerCategoryMin - 1) {
+        this.questionPerCategory++
+      }
+    },
+    questionPerCategoryDec: function () {
+      if (this.questionPerCategory - 1 <= this.questionPerCategoryMax && this.questionPerCategory - 1 >= this.questionPerCategoryMin) {
+        this.questionPerCategory--
+      }
+    },
+    questionForPlayInc: function () {
+      if (this.questionForPlay <= this.questionForPlayMax - 1 && this.questionForPlay >= this.questionForPlayMin - 1) {
+        this.questionForPlay++
+      }
+    },
+    questionForPlayDec: function () {
+      if (this.questionForPlay - 1 <= this.questionForPlayMax && this.questionForPlay - 1 >= this.questionForPlayMin) {
+        this.questionForPlay--
+      }
+    },
     getCategory: function (id, name) {
       this.selectedCategoryId = id
       this.selectedCategoryName = name
@@ -130,6 +187,8 @@ export default {
       this.actQuestion = ''
       this.possAnswers = []
       this.correctAnswer = ''
+      this.questionPerCategory = 10
+      this.questionForPlay = 5
     },
     getRandomQuestions: function () {
       let arr = []
@@ -225,13 +284,18 @@ export default {
   @import '../scss/variables';
   .question {
     &-checkbox {
-      width: 50px;
+      flex: 0 0 50px;
       height: 50px;
       background: $gray-light;
       border-radius: 10px;
       display: inline-block;
       cursor: pointer;
     }
+  }
+
+  .settings-board {
+    width: 400px;
+    margin: 0 auto;
   }
 
   .wrong .question-checkbox{
